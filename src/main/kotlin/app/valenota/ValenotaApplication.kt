a72b086f2711afef393c39ae5cf221608b298a2c
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2023-01-24 20:25:50 -0300

    first commit

---
+++ src/main/kotlin/app/valenota/ValenotaApplication.kt
@@ -0,0 +1,11 @@
+package app.valenota
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ValenotaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ValenotaApplication>(*args)
+}
---
+++ src/test/kotlin/app/valenota/ValenotaApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.valenota
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ValenotaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
