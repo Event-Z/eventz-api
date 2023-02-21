d1108a9e75e11c06cf7e3a551ff94e3920f68db8
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2023-02-21 19:09:45 -0300

    :sparkles: Adiciona criação de pessoa já com endereço

--- src/main/kotlin/app/valenota/model/entity/Person.kt
+++ src/main/kotlin/app/valenota/model/entity/Person.kt
@@ -1,9 +1,12 @@
 package app.valenota.model.entity
 
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToMany
+import jakarta.persistence.OneToOne
 import java.util.UUID
 
 @Entity(name = "person")
@@ -17,6 +20,7 @@ class Person {
     lateinit var cpf: String
     @Column(name = "password")
     lateinit var password: String
+    @OneToOne(cascade = [CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST])
     @JoinColumn(name = "address_id")
     lateinit var address: Address
     constructor(name: String, cpf: String, password: String, address: Address) {
--- src/main/resources/db/db.sql
+++ src/main/resources/db/db.sql
@@ -1,7 +1,7 @@
 create database valenotadb;
 
 create table address (
-	id varchar(32) primary key,
+	id varchar(36) primary key,
     street varchar(255) not null,
     city varchar(255) not null,
     district varchar(255) not null,
@@ -10,53 +10,53 @@ create table address (
 );
 
 create table person (
-	id varchar(32) primary key,
+	id varchar(36) primary key,
 	name varchar(255) not null,
     cpf varchar(11) unique not null,
     password varchar(255) not null,
-    address_id varchar(255) not null,
+    address_id varchar(36) not null,
     foreign key(address_id) references address (id)
 );
 
 create table company (
-	id varchar(32) primary key,
+	id varchar(36) primary key,
 	name varchar(255) unique null,
     cnpj varchar(14) unique null,
     password varchar(255) not null,
-    address_id varchar(255) not null,
+    address_id varchar(36) not null,
     foreign key(address_id) references address (id)
 );
 
 create table support (
-	id varchar(32) primary key,
+	id varchar(36) primary key,
     number varchar(45),
     email varchar(255),
     site varchar(255)
 );
 
 create table event (
-	id varchar(32) primary key,
+	id varchar(36) primary key,
     date_event datetime not null,
     price double not null,
     name varchar(255) not null,
-    company_id varchar(255) not null,
-    support_id varchar(255) not null,
-    address_id varchar(255) not null,
+    company_id varchar(36) not null,
+    support_id varchar(36) not null,
+    address_id varchar(36) not null,
     foreign key(company_id) references company (id),
     foreign key(support_id) references support (id),
     foreign key(address_id) references address (id)
 );
 
 create table person_event (
-	id varchar(32) primary key,
-    person_id varchar(32) not null,
-    event_id varchar(32) not null,
+	id varchar(36) primary key,
+    person_id varchar(36) not null,
+    event_id varchar(36) not null,
 	foreign key(person_id) references person (id),
     foreign key(event_id) references event (id)
 );
 
 create table question (
-	id varchar(32) primary key,
+	id varchar(36) primary key,
     q varchar(255) not null,
     resp varchar(255) not null
 );
