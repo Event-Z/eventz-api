b86b2fc8ec7d8d7d59331d2bafd662d38eeafe2b
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2023-07-05 01:14:50 -0300

    config: Adiciona main class
    Signed-off-by: LucasSalesMoreira <lucasdevsoftware@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,3 +44,21 @@ tasks.withType<Test> {
 springBoot {
 	mainClass.set("app.valenota.ValenotaApplication")
 }
+
+tasks.withType<Jar> {
+	// Otherwise you'll get a "No main manifest attribute" error
+	manifest {
+		attributes["Main-Class"] = "app.valenota.ValenotaApplication"
+	}
+
+	// To avoid the duplicate handling strategy error
+	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+	// To add all of the dependencies
+	from(sourceSets.main.get().output)
+
+	dependsOn(configurations.runtimeClasspath)
+	from({
+		configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+	})
+}
