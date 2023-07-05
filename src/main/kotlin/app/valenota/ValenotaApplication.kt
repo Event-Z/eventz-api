225e0ce9ba9343dbbb022eef48fa67abfa3c91e1
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2023-07-05 00:19:34 -0300

    config: Adiciona main class
    Signed-off-by: LucasSalesMoreira <lucasdevsoftware@gmail.com>

--- src/main/kotlin/app/valenota/ValenotaApplication.kt
+++ src/main/kotlin/app/valenota/ValenotaApplication.kt
@@ -3,9 +3,17 @@ package app.valenota
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
 class ValenotaApplication
-
+/*
 fun main(args: Array<String>) {
 	runApplication<ValenotaApplication>(*args)
+}*/
+
+object Main {
+	@JvmStatic
+	fun main(args: Array<String>) {
+		runApplication<ValenotaApplication>(*args)
+	}
 }
