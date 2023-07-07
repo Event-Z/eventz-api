00c38341d8b48dee502dbaed78647c79c5c210e7
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2023-07-06 22:19:19 -0300

    feat: update configs
    Signed-off-by: LucasSalesMoreira <lucasdevsoftware@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,24 +41,14 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
+/*
 springBoot {
 	mainClass.set("app.valenota.ValenotaApplication")
-}
+}*/
 
 tasks.withType<Jar> {
 	// Otherwise you'll get a "No main manifest attribute" error
 	manifest {
 		attributes["Main-Class"] = "app.valenota.ValenotaApplication"
 	}
-
-	// To avoid the duplicate handling strategy error
-	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-
-	// To add all of the dependencies
-	from(sourceSets.main.get().output)
-
-	dependsOn(configurations.runtimeClasspath)
-	from({
-		configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-	})
 }
