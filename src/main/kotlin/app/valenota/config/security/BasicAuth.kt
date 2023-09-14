d0a188d838790825ce499b01eacdc5458686d84f
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2023-09-14 20:43:03 -0300

    feat: Adiciona tratamento de cors
    Signed-off-by: LucasSalesMoreira <lucasdevsoftware@gmail.com>

--- src/main/kotlin/app/valenota/config/security/BasicAuth.kt
+++ src/main/kotlin/app/valenota/config/security/BasicAuth.kt
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -21,5 +24,19 @@ class BasicAuth {
             .csrf()
             .ignoringRequestMatchers("/**")
         return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val corsConfiguration = CorsConfiguration()
+        corsConfiguration.allowedOrigins = listOf("*")
+        corsConfiguration.allowedMethods = listOf("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD")
+        corsConfiguration.allowCredentials = true
+        corsConfiguration.maxAge = 3600L
+        corsConfiguration.exposedHeaders = listOf("X-Get-Header")
+        corsConfiguration.allowedHeaders = listOf("Authorization", "Requestor-Type")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfiguration)
+        return source
     }
 }
